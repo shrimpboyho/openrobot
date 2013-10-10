ea44754dadf6f83f07022df8894eff27c7205441
shrimpboyho <shrimpboyhoftw@gmail.com>
2013-10-10 18:37:39 -0400

    Working on making function prototypes better

--- output.c
+++ output.c
@@ -1,10 +1,9 @@
 /* Function prototypes */
-def move():
-def main():
-def junk(crap):
+int move();
+string junk(crap);
 
 /* Global scope */
-int speed = 4
-float motor = 69.5
-string text = "hey +  guys!"
-int junk =   5    +4
+int speed = 4;
+float motor = 69.5;
+string text = "hey +  guys!";
+int junk =   5    +4;
